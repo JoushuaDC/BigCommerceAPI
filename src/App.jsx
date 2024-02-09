ccca98cf116fdd4d4b2750eb5ccf7bda28361349
Joushua DC <joushua.d@algaecal.com>
2024-02-08 19:21:31 -0800

    Added the stylesheets

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import NavBar from "./components/navbar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const navbar = () => {
+  return <header>navbar</header>;
+};
+
+export default navbar;
